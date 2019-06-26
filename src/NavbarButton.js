4071eda7808f29acfaa2b7d9a5bf6873330318cd
Matt McNaney <mcnaneym@appstate.edu>
2019-06-26 10:31:40 -0400

    Added margin to button if marginButton prop == true
    + Fixed undefined class echo
    + default color changed from "default" to "outline-secondary"

--- src/NavbarButton.js
+++ src/NavbarButton.js
@@ -2,36 +2,40 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-const NavbarButton = (props) => {
+const NavbarButton = props => {
   const getClass = () => {
-    let size
+    let size = ''
     switch (props.size) {
       case 'lg':
       case 'sm':
-        size = 'btn-' + props.size
+        size = ' btn-' + props.size
         break
     }
 
-    return `btn ${size} btn-${props.color}`
+    return `btn${size} btn-${props.color}${props.marginRight ? ' mr-2' : ''}`
   }
 
   return (
     <div>
-      <button className={getClass()} onClick={props.handleClick}>{props.label}</button>
+      <button className={getClass()} onClick={props.handleClick}>
+        {props.label}
+      </button>
     </div>
   )
 }
 
 NavbarButton.propTypes = {
-  label: PropTypes.oneOfType([PropTypes.string, PropTypes.object,]),
+  label: PropTypes.oneOfType([PropTypes.string, PropTypes.object]),
   handleClick: PropTypes.func,
   color: PropTypes.string,
-  size: PropTypes.string
+  size: PropTypes.string,
+  marginRight: PropTypes.bool
 }
 
 NavbarButton.defaultProps = {
-  color: 'default',
-  size: null
+  color: 'outline-secondary',
+  size: null,
+  marginRight: true
 }
 
 export default NavbarButton
